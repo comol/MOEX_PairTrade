0bf12a59e46cc1cde3a9dae57c6f2f0c659bd818
comol <comol@mail.ru>
2020-02-28 03:14:26 +0300

    initial commit

---
+++ VS/Cont/NN3/CountinueHour.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+using TSLab.DataSource;
+using TSLab.Script.Helpers;
+
+namespace Hour
+{
+
+    [HandlerCategory("NN")]
+    [HandlerName("ContinueHour")]
+
+    public class ContinueHourClass : IOneSourceHandler, ISecurityInputs, ISecurityReturns, IStreamHandler, IContextUses
+    {
+
+        public IContext Context { set; private get; }
+
+        public ISecurity Execute(ISecurity sec)
+        {
+
+            var ValCount = sec.Bars.Count;
+            int prevhour = 0;
+            IList<Bar> NewBars = new List<Bar>();
+            
+            foreach(var bar in sec.Bars)
+            {
+                
+                if(bar.Date.Hour - prevhour  > 1 && prevhour > 0)
+                {
+                    for (int i = 1; i < bar.Date.Hour - prevhour; i++)
+                    {
+                        Bar newbar = new Bar(bar.Color, bar.Date.AddHours(1), bar.Open, bar.High, bar.Low, bar.Close, bar.Volume);
+                        NewBars.Add(newbar);
+                    }
+                }
+                NewBars.Add(bar);
+                prevhour = bar.Date.Hour;
+            }
+
+            ISecurity newsec = sec.CloneAndReplaceBars(NewBars);
+            return newsec;
+        }
+
+    }
+}
---
+++ VS/NN3/NN3Class.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using MathWorks.MATLAB.NET.Arrays;
+using MathWorks.MATLAB.NET.Utility;
+using NNLib;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+using TSLab.DataSource;
+using TSLab.Script.Helpers;
+using System.IO;
+
+// Инструменты
+// BR
+// Eu
+// GAZR 
+// GMKR
+// GOLD
+// LKOH
+// MGNT
+// MIX
+// ROSN
+// RTS
+// Si
+
+
+namespace NN3
+{
+
+    [HandlerCategory("NN")]
+    [HandlerName("NN PairTrade")]
+    [InputsCount(2, 200)]
+    [InputInfo(0, "Инструмент")]
+
+    public class NN3Class : ITwoSourcesHandler, IDoubleReturns, IStreamHandler, IContextUses, IValuesHandlerWithNumber
+    {
+
+        [HandlerParameter(Name = "Отладка", Default = "false", NotOptimized = true)]
+        public bool Debug { get; set; }
+
+        public IContext Context { set; private get; }
+
+        public IList<double> Execute(params IList<double>[] secArr)
+        {
+            
+            var Filename = "c:\\Temp\\" + Guid.NewGuid().ToString() + ".txt";
+            if (Debug == true)
+            {
+                
+            }
+
+            var ValCount = secArr[0].Count;
+            var SymbolsCount  = 11;
+            var values = new double[ValCount];
+            NNClass NNMatlab = new NNClass();
+            MWNumericArray NNres;
+            MWNumericArray x2 = new double[SymbolsCount];
+            double NNtotal;
+
+            for (var i = 0; i < ValCount; i++)
+            {
+                MWArray result;
+                for (var j = 0; j < SymbolsCount; j++)
+                {
+                    int LocalValCount = secArr[j].Count; //Если есть пропуски то берём последнее
+                    if (i >= LocalValCount)
+                    {
+                        x2[j + 1] = secArr[j][LocalValCount-1];
+                    }
+                    else
+                    {
+                        x2[j + 1] = secArr[j][i];
+                    }
+                }
+                //x2[7] = 0; //!!!!!!!! ЗАГРУШКА!!!!!! 
+                result = NNMatlab.NNFunc(x2);
+                NNres = (MWNumericArray)result[1];
+                NNtotal = (double)NNres[1];
+                values[i] = NNtotal;
+
+                if (Debug)
+                {
+                    StreamWriter outputFile = File.AppendText(Filename); ;
+                    string TextToWrite = "";
+                    for (var j = 0; j < SymbolsCount; j++)
+                    {
+                        TextToWrite = TextToWrite + ", " + x2[j + 1].ToString();
+                    }
+                    TextToWrite = TextToWrite + ", " + NNtotal.ToString();
+                    outputFile.WriteLine(TextToWrite);
+                    outputFile.Close();
+                }
+
+            }
+
+          
+            return values;
+        }
+
+    }
+}
